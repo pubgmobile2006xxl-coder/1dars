2e40156967d06a623040a0191f4c3b2e7b6e187d
AZIZBEK <pubgmobile2006xxl@gmail.com>
2026-04-28 20:51:28 +0500

    cdcd

---
+++ Новая папка/vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+import HEader from "./Components/HEADEER/header"
+import log from "../src/img/log.png"
+function App() {
+    return (
+        <>
+            <HEADER></HEADER>
+        </>
+    )
+}
+
+export default App
